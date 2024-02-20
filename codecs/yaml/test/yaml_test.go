c8c8b9b01d3bc1053fcfbd672aa95b3939d3d10c
hz <hedzrz@gmail.com>
2024-02-20 09:38:06 +0800

    codec.yaml: added another test

--- codecs/yaml/test/yaml_test.go
+++ codecs/yaml/test/yaml_test.go
@@ -29,3 +29,24 @@ func TestNew(t *testing.T) {
 	assert.Equal(t, `-w`, s.MustGet("app.yaml.app.bgo.build.projects.000-default-group.items.001-bgo.ldflags.1"))
 
 }
+
+func TestNew2(t *testing.T) {
+	s := store.New()
+	parser := yaml.New()
+	if _, err := s.Load(context.TODO(),
+		store.WithStorePrefix("app.yaml"),
+		store.WithCodec(parser),
+		store.WithProvider(
+			file.New("../../../testdata/2.yaml",
+				file.WithPosition("app.bgo"),
+			)),
+
+		store.WithStoreFlattenSlice(true), // decode and flatten slice into tree structure instead treat it as a simple value
+	); err != nil {
+		t.Fatalf("Load failed: %v", err)
+	}
+	t.Logf("\n%-32sData\n%v\n", "Path", s.Dump())
+
+	assert.Equal(t, `-s`, s.MustGet("app.yaml.app.bgo.build.projects.000-default-group.items.001-bgo.ldflags.0"))
+	assert.Equal(t, `-w`, s.MustGet("app.yaml.app.bgo.build.projects.000-default-group.items.001-bgo.ldflags.1"))
+}
