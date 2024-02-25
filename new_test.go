04aa47fa2c116c10ffe233c84379c7026570b9a6
hz <hedzrz@gmail.com>
2024-02-25 08:35:14 +0800

    updated tests

--- new_test.go
+++ new_test.go
@@ -1,10 +1,274 @@
 package store
 
 import (
+	"context"
+	"strings"
+	"sync/atomic"
 	"testing"
+
+	"github.com/hedzr/store/internal/cvt"
 )
 
 func TestRandomStringPure(t *testing.T) {
 	// t.Log(stringtool.RandomStringPure(8))
 	t.Log("End.")
+}
+
+func TestWithinLoading(t *testing.T) {
+	conf := newBasicStore()
+
+	defer conf.Close()
+
+	t.Logf("\nPath of 'conf' (delimeter=%v, prefix=%v)\n%v\n",
+		conf.Delimiter(),
+		conf.Prefix(),
+		conf.Dump())
+
+	conf.WithinLoading(func() {
+		t.Log("")
+	})
+
+	assertEqual(t, 5, conf.MustInt("app.server.start"))
+	assertEqual(t, 6, conf.MustInt("app.logging.rotate", -1))
+
+	// load with the explicit empty provider and codec
+
+	ctx := context.TODO()
+	_, err := conf.Load(ctx,
+		WithCodec(nil),
+		WithProvider(nil),
+		WithStoreFlattenSlice(false),
+		WithStorePrefix(""),
+		WithPosition(""),
+	)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	conf.Remove("app.logging")
+
+	assertEqual(t, 5, conf.MustInt("app.server.start"))
+	assertEqual(t, -1, conf.MustInt("app.logging.rotate", -1))
+
+	t.Logf("\nPath of 'conf' (delimeter=%v, prefix=%v)\n%v\n",
+		conf.Delimiter(),
+		conf.Prefix(),
+		conf.Dump())
+}
+
+func TestStoreS_join(t *testing.T) {
+	// join ...
+
+	conf := newBasicStore()
+	assertEqual(t, "", conf.join())
+	assertEqual(t, "", conf.join(""))
+	assertEqual(t, "x", conf.join("x", ""))
+	assertEqual(t, "x.y", conf.join("x", "y"))
+	assertEqual(t, "x.y", conf.join("x", "y", ""))
+	assertEqual(t, "x.y.z", conf.join("x", "y", "z"))
+	assertEqual(t, "x.y.z", conf.join("x", "y", "z", ""))
+	assertEqual(t, "x.y.z", conf.join("", "x", "y", "z", ""))
+}
+
+func TestOp_Marshal(t *testing.T) {
+	var op Op
+	for k, v := range opStrings {
+		err := (&op).UnmarshalText([]byte(v))
+		if err != nil {
+			t.Fatalf("failed: %v", err)
+		}
+		if op != k {
+			t.Fatalf("unmarshaltext failed, expecting '%v' but got '%v'", k, op)
+		}
+
+		b, err := op.MarshalText()
+		if err != nil {
+			t.Fatalf("failed: %v", err)
+		}
+		t.Logf("marshal text: %v", string(b))
+
+		_ = op.Marshal()
+	}
+
+	err := (&op).UnmarshalText([]byte("???"))
+	if err == nil {
+		t.Fail()
+	}
+
+	op = Op(65535)
+	v, _ := op.MarshalText()
+	if !strings.HasPrefix(string(v), "Op(") {
+		t.Fail()
+	}
+}
+
+func TestStoreS_Load(t *testing.T) {
+	conf := newBasicStore(WithWatchEnable(true))
+	defer conf.Close()
+
+	// load with a small copy of maps provider, it's inline here for testing only
+
+	m := map[string]any{
+		"m1.s1": "cool", // a key containing delimiter should be splitted as children automatically
+		"m1.s2": 9,
+		"key2": map[any]any{
+			9: 1,
+			8: false,
+		},
+		"slice": []map[any]any{
+			{7.981: true, "st": true, "cool": "maps"},
+			{"hello": "world"},
+		},
+	}
+	ctx := context.TODO()
+	_, err := conf.Load(ctx,
+		WithProvider(newMaps(m, ".")),
+		WithStorePrefix("app.maps"),
+		WithPosition(""),
+		WithStoreFlattenSlice(true), // this option allows 'slice' to split as child nodes
+	)
+	if ErrorIsNotFound(err) {
+		t.Fail()
+	}
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	t.Logf("\nPath of 'conf' (delimeter=%v, prefix=%v)\n%v\n",
+		conf.Delimiter(),
+		conf.Prefix(),
+		conf.Dump())
+
+	assertEqual(t, false, conf.MustBool("app.maps.key2.8"))
+	assertEqual(t, 1, conf.MustInt("app.maps.key2.9", -1))
+	assertEqual(t, "cool", conf.MustString("app.maps.m1.s1"))
+	assertEqual(t, 9, conf.MustInt("app.maps.m1.s2", -1))
+	// assertEqual(t, true, conf.MustBool("app.maps.slice.0.7.981", false))
+	assertEqual(t, "true", conf.MustString("app.maps.slice.0.st"))
+}
+
+//
+
+//
+
+//
+
+func newMaps(m map[string]any, delimiter string, opts ...mapsOpt) *pvdr { //nolint:revive
+	s := &pvdr{delimiter: delimiter}
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	cp := cvt.Normalize(cvt.Copy(m), nil)
+	if s.delimiter != "" {
+		cp = cvt.Deflate(cp, s.delimiter)
+	}
+
+	// s.m = cp
+
+	s.m = make(map[string]ValPkg)
+	for k, v := range cp {
+		s.m[k] = ValPkg{
+			Value:   v,
+			Desc:    "",
+			Comment: "",
+			Tag:     nil,
+		}
+	}
+	return s
+}
+
+type mapsOpt func(s *pvdr)
+
+type pvdr struct {
+	m         map[string]ValPkg
+	delimiter string
+	prefix    string
+	codec     Codec
+	watching  int32
+}
+
+func mapsWithCodec(codec Codec) mapsOpt {
+	return func(s *pvdr) {
+		s.codec = codec
+	}
+}
+
+func mapsWithPosition(prefix string) mapsOpt {
+	return func(s *pvdr) {
+		s.prefix = prefix
+	}
+}
+
+func mapsWithDelimiter(d string) mapsOpt {
+	return func(s *pvdr) {
+		s.delimiter = d
+	}
+}
+
+func (s *pvdr) GetCodec() (codec Codec)    { return s.codec }
+func (s *pvdr) GetPosition() (pos string)  { return s.prefix }
+func (s *pvdr) WithCodec(codec Codec)      { s.codec = codec }
+func (s *pvdr) WithPosition(prefix string) { s.prefix = prefix }
+
+func (s *pvdr) Count() int {
+	return 0
+}
+
+func (s *pvdr) Has(key string) bool { //nolint:revive
+	return false
+}
+
+func (s *pvdr) Next() (key string, eol bool) {
+	eol = true
+	return
+}
+
+func (s *pvdr) Keys() (keys []string, err error) {
+	err = ErrNotImplemented
+	return
+}
+
+func (s *pvdr) Value(key string) (value any, ok bool) { //nolint:revive
+	ok = false
+	return
+}
+
+func (s *pvdr) MustValue(key string) (value any) { //nolint:revive
+	return
+}
+
+func (s *pvdr) Reader() (r Reader, err error) { //nolint:revive
+	err = ErrNotImplemented
+	return
+}
+
+// Read returns the loaded map[string]interface{}.
+func (s *pvdr) Read() (data map[string]ValPkg, err error) {
+	return s.m, nil
+}
+
+// ReadBytes is not supported by the confmap provider.
+func (s *pvdr) ReadBytes() (data []byte, err error) {
+	err = ErrNotImplemented
+	return
+}
+
+func (s *pvdr) Write(data []byte) (err error) { //nolint:revive
+	err = ErrNotImplemented
+	return
+}
+
+func (s *pvdr) Close() {
+	atomic.CompareAndSwapInt32(&s.watching, 1, 0)
+}
+
+func (s *pvdr) Watch(ctx context.Context, cb func(event any, err error)) (err error) {
+	if !atomic.CompareAndSwapInt32(&s.watching, 0, 1) {
+		return
+	}
+
+	// do some stuff here
+
+	return
 }
--- store_test.go
+++ store_test.go
@@ -7,25 +7,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/hedzr/store/ctx"
-	"github.com/hedzr/store/internal/radix"
+	"github.com/hedzr/store/radix"
 )
-
-func TestCtxS_NamesCount(t *testing.T) {
-	ctx := ctx.TODO() //nolint:gocritic
-	for ctx.Next() {
-		t.Log(ctx.Key())
-	}
-}
-
-func TestCtxS_Next(t *testing.T) {
-	ctx := ctx.WithValue(ctx.TODO(), "k1", 1)
-	ctx.WithValues("k2", 2, "k3", 3)
-
-	for ctx.Next() {
-		t.Log(ctx.Key())
-	}
-}
 
 func TestStore_Get(t *testing.T) {
 	trie := NewStoreT[any]()
@@ -53,18 +36,18 @@ func TestStore_Get(t *testing.T) {
 	}{
 		{"app.debug", true, 1},
 		{"app.dump", true, 3},
-		{"app.d", false, nil},
-		{"app.de", false, nil},
-		{"app.deb", false, nil},
+		{"app.d", false, nil},   // partial-matched
+		{"app.de", false, nil},  // non-exist key
+		{"app.deb", false, nil}, // no-exist key
 		{"app.verbose", true, 2},
 		{"app.logging", true, nil},
 		{"app.logging.", true, nil},
 		{"app.logging.file", true, 4},
 		{"app.logging.rotate", true, 6},
 		{"app.server.start", true, 5},
-		{"app", true, nil},
-		{"app.", true, nil},
-		{"a", false, nil},
+		{"app", true, nil},  // a branch key
+		{"app.", true, nil}, // a branch key with ending '.'
+		{"a", false, nil},   // a missed key
 	} {
 		if data, found := trie.Get(c.query); found == c.found && data == c.data {
 			continue
@@ -74,32 +57,7 @@ func TestStore_Get(t *testing.T) {
 	}
 }
 
-func TestStore_Set(t *testing.T) {
-	// trie := NewStoreT[any]()
-	trie := newBasicStore()
-	trie.Set("app.debug.map", map[string]any{"tags": []string{"delve", "verbose"}, "verbose": true})
-	t.Logf("\nPath\n%v\n", trie.Dump())
-}
-
-func TestStore_Merge(t *testing.T) {
-	// trie := NewStoreT[any]()
-	trie := newBasicStore()
-	if err := trie.Merge("app.debug.map", map[string]any{"tags": []string{"delve", "verbose"}, "verbose": true}); err != nil {
-		t.Fatalf(`Merge failed: %v`, err)
-	}
-	t.Logf("\nPath\n%v\n", trie.Dump())
-
-	assertTrue(t, slices.Equal(trie.MustGet("app.debug.map.tags").([]string), []string{"delve", "verbose"}), `expecting trie.Get("app.debug.map.tags") return '[delve verbose]'`)
-}
-
-func TestStore_WithPrefix(t *testing.T) {
-	trie := newBasicStore()
-	t.Logf("\nPath\n%v\n", trie.Dump())
-
-	assertTrue(t, trie.MustGet("app.logging.rotate") == 6, `expecting trie.Get("app.logging.rotate") return 6`)
-}
-
-func TestStore_Get2(t *testing.T) {
+func TestStore_Get2(t *testing.T) { // test data with `reflect.DeepEqual`, tests for slice or map
 	conf := newBasicStore()
 
 	// ret := conf.dump()
@@ -151,33 +109,152 @@ func TestStore_GetM(t *testing.T) {
 
 	// filter by functor
 
-	m, err = conf.GetM("", WithFilter[any](func(node radix.Node[any]) bool {
-		return strings.HasPrefix(node.Key(), "app.logging.")
-	}))
+	m, err = conf.GetM("",
+		WithKeepPrefix[any](false),
+		WithoutFlattenKeys[any](false),
+		WithFilter[any](func(node radix.Node[any]) bool {
+			return strings.HasPrefix(node.Key(), "app.logging.")
+		}))
 	if err != nil {
 		t.Fatalf("wrong in calling GetM(\"\"): %v", err)
 	}
 	t.Logf("app.logging sub-tree: %v", m)
 }
 
+func TestStore_GetSectionFrom(t *testing.T) {
+	conf := newBasicStore()
+	conf.Set("app.logging.words", []any{"a", 1, false})
+	conf.Set("app.server.sites", -1)
+	t.Logf("\nPath\n%v\n", conf.Dump())
+
+	type loggingS struct {
+		File   uint
+		Rotate uint64
+		Words  []any
+	}
+
+	type serverS struct {
+		Start int
+		Sites int
+	}
+
+	type appS struct {
+		Debug   int
+		Dump    int
+		Verbose int64
+		Logging loggingS
+		Server  serverS
+	}
+
+	type cfgS struct {
+		App appS
+	}
+
+	var ss cfgS
+	err := conf.GetSectionFrom("", &ss)
+	t.Logf("cfgS: %v | err: %v", ss, err)
+
+	assertEqual(t, []any{"a", 1, false}, ss.App.Logging.Words)
+	assertEqual(t, -1, ss.App.Server.Sites)
+
+	if !reflect.DeepEqual(ss.App.Logging.Words, []any{"a", 1, false}) {
+		t.Fail()
+	}
+
+	err = conf.GetSectionFrom("nonexist", nil)
+	t.Log("nothing happened")
+
+	err = conf.GetSectionFrom("nonexist", &ss)
+	t.Logf("cfgS: %v | err: %v", ss, err)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestStore_Set(t *testing.T) {
+	// trie := NewStoreT[any]()
+	trie := newBasicStore()
+	trie.Set("app.debug.map", map[string]any{"tags": []string{"delve", "verbose"}, "verbose": true})
+	t.Logf("\nPath\n%v\n", trie.Dump())
+}
+
+func TestStore_Merge(t *testing.T) {
+	// trie := NewStoreT[any]()
+	trie := newBasicStore()
+	if err := trie.Merge("app.debug.map", map[string]any{"tags": []string{"delve", "verbose"}, "verbose": true}); err != nil {
+		t.Fatalf(`Merge failed: %v`, err)
+	}
+	t.Logf("\nPath\n%v\n", trie.Dump())
+
+	assertTrue(t, slices.Equal(trie.MustGet("app.debug.map.tags").([]string), []string{"delve", "verbose"}), `expecting trie.Get("app.debug.map.tags") return '[delve verbose]'`)
+}
+
+func TestStore_WithPrefix(t *testing.T) {
+	trie := newBasicStore()
+	t.Logf("\nPath\n%v\n", trie.Dump())
+
+	assertEqual(t, 6, trie.MustGet("app.logging.rotate"))
+	conf := trie.WithPrefix("app")
+	assertEqual(t, 6, conf.MustGet("logging.rotate"))
+	conf = conf.WithPrefix("logging")
+	assertEqual(t, 6, conf.MustGet("rotate"))
+	conf = trie.WithPrefixReplaced("app.logging")
+	assertEqual(t, 6, conf.MustGet("rotate"))
+}
+
 func TestStore_Walk(t *testing.T) {
 	var conf Store = newBasicStore()
-	conf.Walk("", func(prefix, key string, node radix.Node[any]) {
-		t.Logf("%v / %v => %v", prefix, key, node)
+	conf.Walk("", func(path, fragment string, node radix.Node[any]) {
+		t.Logf("%v / %v => %v", path, fragment, node)
 	})
 }
 
 func TestStore_Walk1(t *testing.T) {
 	var conf Store = newBasicStore()
-	conf.Walk("app", func(prefix, key string, node radix.Node[any]) {
-		t.Logf("%v / %v => %v", prefix, key, node)
+	conf.Walk("app", func(path, fragment string, node radix.Node[any]) {
+		t.Logf("%v / %v => %v", path, fragment, node)
 	})
 }
 
 func TestStore_Walk2(t *testing.T) {
-	var conf Store = newBasicStore()
-	conf.Walk("app.", func(prefix, key string, node radix.Node[any]) {
-		t.Logf("%v / %v => %v", prefix, key, node)
+	var conf Store = newBasicStore(
+		WithDelimiter('.'),
+		WithPrefix(""),
+		WithFlattenSlice(true),
+		WithWatchEnable(false),
+	)
+	defer conf.Close()
+
+	_ = conf.MustGet("x")
+	_ = conf.Remove("x")
+	_ = conf.Has("x")
+	_, _, _, _ = conf.Locate("x")
+	_ = conf.Clone()
+	_ = conf.Dup()
+
+	conf.SetPrefix(conf.Prefix())
+
+	_ = conf.(*storeS).loadMap(map[string]any{
+		"m1": map[string]any{
+			"s1": "s1",
+			"s2": 2,
+		},
+		"m2": []map[string]any{
+			{
+				"s1": "s1",
+				"s2": 2,
+			},
+			{
+				"s1": "s1",
+				"s2": 2,
+			},
+		},
+		"m3": []any{1, 2, false},
+		"m4": 3.1313,
+	}, "app", true, nil)
+
+	conf.Walk("app.", func(path, fragment string, node radix.Node[any]) {
+		t.Logf("%v / %v => %v", path, fragment, node)
 	})
 }
 
@@ -196,9 +273,14 @@ func TestStore_Monitoring(t *testing.T) {
 
 	t.Logf("tests begins.")
 	conf.Set("app.server.port", 7300)
+	assertEqual(t, 7300, conf.MustInt("app.server.port", -1))
+	assertTrue(t, 7300 == conf.MustInt("app.server.port", -1))
 	conf.Set("app.server.port", 7301)
+	assertEqual(t, 7301, conf.MustInt("app.server.port", -1))
+	assertFalse(t, 7300 == conf.MustInt("app.server.port", -1))
 	conf.Set("app.server.tls.cert", "/tmp/cert.pem")
 	conf.Set("app.server.tls.priv", "/tmp/private-key.pem")
+	assertEqual(t, "/tmp/private-key.pem", conf.MustString("app.server.tls.priv"))
 	node, np, removed := conf.RemoveEx("app.server.tls.cert")
 	t.Logf("node %q (parent: %v) removed %v: %v", node.Key(), np.Key(), removed, node)
 	conf.Set("app.server.tls.priv", "/tmp/private-key.pem")
@@ -208,6 +290,52 @@ func TestStore_Monitoring(t *testing.T) {
 //
 
 //
+
+func TestStoreS_Get(t *testing.T) {
+	trie := newBasicStore()
+	fmt.Println(trie.MustInt("app.dump"))
+	fmt.Println(trie.MustString("app.dump"))
+	fmt.Println(trie.MustBool("app.dump"))
+	// Output:
+	// 3
+	// 3
+	// true
+	assertEqual(t, 3, trie.MustInt("app.dump"))
+	assertEqual(t, "3", trie.MustString("app.dump"))
+	assertEqual(t, true, trie.MustBool("app.dump"))
+}
+
+func TestStoreS_Dump(t *testing.T) {
+	conf := New()
+	conf.Set("app.debug", false)
+	conf.Set("app.verbose", true)
+	conf.Set("app.dump", 3)
+	conf.Set("app.logging.file", "/tmp/1.log")
+	conf.Set("app.server.start", 5)
+
+	ss := conf.WithPrefix("app.logging")
+	ss.Set("rotate", 6)
+	ss.Set("words", []any{"a", 1, false})
+
+	data, found := conf.Get("app.logging.rotate")
+	assertEqual(t, 6, data)
+	assertEqual(t, true, found)
+	data = conf.MustGet("app.logging.rotate")
+	assertEqual(t, 6, data)
+
+	assertEqual(t, 3, conf.MustInt("app.dump"))
+	assertEqual(t, "3", conf.MustString("app.dump"))
+	assertEqual(t, true, conf.MustBool("app.dump"))
+
+	s2, e2 := conf.GetStringSlice("app.logging.words")
+	assertEqual(t, nil, e2)
+	assertEqual(t, []string{"a", "1", "false"}, s2)
+	assertEqual(t, []string{"a", "1", "false"}, conf.MustStringSlice("app.logging.words"))
+	assertEqual(t, []int{0, 1, 0}, conf.MustIntSlice("app.logging.words"))
+	assertEqual(t, map[string]any{"words": []any{"a", 1, false}}, conf.MustM("app.logging.words"))
+
+	t.Logf("%v", conf.Dump())
+}
 
 //
 
@@ -278,5 +406,5 @@ func newBasicStore(opts ...Opt) *storeS {
 	ss := conf.WithPrefix("app.logging")
 	ss.Set("rotate", 6)
 	ss.Set("words", []string{"a", "1", "false"})
-	return conf
+	return conf.(*storeS)
 }
