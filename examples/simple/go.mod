53ace78d795c8a2f869f7ad5200246e4382c2449
hz <hedzrz@gmail.com>
2024-03-18 10:09:17 +0800

    added examples/simple as an cli-app

---
+++ examples/simple/go.mod
@@ -0,0 +1,53 @@
+module github.com/hedzr/store/examples/simple
+
+go 1.22
+
+// replace gopkg.in/hedzr/errors.v3 => ../../../../24/libs.errors
+
+// replace github.com/hedzr/evendeep => ../../../libs.diff
+
+// replace github.com/hedzr/go-errors/v2 => ../../../libs.errors
+
+// replace github.com/hedzr/env => ../../../libs.env
+
+// replace github.com/hedzr/is => ../../../libs.is
+
+// replace github.com/hedzr/logg => ../../../libs.logg
+
+replace github.com/hedzr/store => ../../
+
+replace github.com/hedzr/store/codecs/json => ../../codecs/json
+
+replace github.com/hedzr/store/codecs/yaml => ../../codecs/yaml
+
+replace github.com/hedzr/store/providers/consul => ../../providers/consul
+
+replace github.com/hedzr/store/providers/env => ../../providers/env
+
+replace github.com/hedzr/store/providers/etcd => ../../providers/etcd
+
+replace github.com/hedzr/store/providers/file => ../../providers/file
+
+replace github.com/hedzr/store/providers/flags => ../../providers/flags
+
+replace github.com/hedzr/store/providers/fs => ../../providers/fs
+
+replace github.com/hedzr/store/providers/maps => ../../providers/maps
+
+require (
+	github.com/arl/statsviz v0.6.0
+	github.com/hedzr/store v0.5.1
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hedzr/evendeep v1.1.6 // indirect
+	github.com/hedzr/is v0.5.17 // indirect
+	github.com/hedzr/logg v0.5.13 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.18.0 // indirect
+	gopkg.in/hedzr/errors.v3 v3.3.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ examples/simple/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/arl/statsviz"
+
+	"github.com/hedzr/is"
+
+	"github.com/hedzr/store"
+
+	"net/http/pprof"
+)
+
+func main() {
+	println(`
+> To view the instant heap stats graph, open the page:
+
+    http://localhost:8080/debug/statsviz
+
+> To view the pprof endpoints, try to run this command in a shell:
+
+    $ go tool pprof -http=:6060 http://localhost:8080/debug/pprof/heap
+
+> Refs:
+  - https://hedzr.com/golang/profiling/golang-pprof/ (Chinese)
+  - https://stackoverflow.com/questions/24863164/how-to-analyze-golang-memory
+  - https://pkg.go.dev/runtime#MemStats
+  - https://pkg.go.dev/net/http/pprof
+
+	`)
+
+	svr := prepareHTTPServer()
+	conf := prepareStore()
+
+	// go func() { log.Println(http.ListenAndServe("localhost:8080", mux)) }()
+
+	catcher := is.Signals().Catch()
+	catcher.
+		WithOnSignalCaught(func(sig os.Signal, wg *sync.WaitGroup) {
+			println()
+			_, _ = sig, wg
+			// logger.Debug("signal caught", "sig", sig)
+			if err := svr.Shutdown(context.TODO()); err != nil {
+				wg.Done() // just for programmatic safety
+				log.Fatal("server shutdown error", "err", err)
+			}
+			wg.Done() // http server shutdown ok, so closing catcher's wg counter.
+			testStore(conf)
+		}).
+		Wait(func(stopChan chan<- os.Signal, wgShutdown *sync.WaitGroup) {
+			// server.Debug("entering looper's loop...")
+			go func() {
+				_, _ = stopChan, wgShutdown
+				err := svr.ListenAndServe()
+				if err != nil {
+					log.Fatal("server serve failed", "err", err)
+				}
+			}()
+		})
+}
+
+func prepareHTTPServer() *http.Server {
+	mux := http.NewServeMux()
+	mux.Handle("/debug/pprof/allocs", pprof.Handler("allocs"))
+	mux.Handle("/debug/pprof/block", pprof.Handler("block"))
+	mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	mux.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
+
+	// see: https://github.com/arl/statsviz
+	// another: https://github.com/go-echarts/statsview
+	_ = statsviz.Register(mux) // check the debug page at: http://localhost:8080/debug/statsviz
+
+	svr := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	return svr
+}
+
+func prepareStore() store.Store {
+	conf := newBasicStore()
+	conf.Set("app.logging.words", []any{"a", 1, false})
+	conf.Set("app.server.sites", -1)
+	return conf
+}
+
+func testStore(conf store.Store) {
+	fmt.Printf("\nPath\n%v\n", conf.Dump())
+}
+
+func newBasicStore(opts ...store.Opt) store.Store {
+	conf := store.New(opts...)
+	conf.Set("app.debug", false)
+	conf.Set("app.verbose", true)
+	conf.Set("app.dump", 3)
+	conf.Set("app.logging.file", "/tmp/1.log")
+	conf.Set("app.server.start", 5)
+
+	// conf.Set("app.logging.rotate", 6)
+	// conf.Set("app.logging.words", []string{"a", "1", "false"})
+
+	ss := conf.WithPrefix("app.logging")
+	ss.Set("rotate", 6)
+	ss.Set("words", []string{"a", "1", "false"})
+	return conf
+}
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
